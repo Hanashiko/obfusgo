ee40280d0f102a9abc325d75438be5d0cfb6981e
Hanashiko <hlichisper@gmail.com>
2025-11-08 00:44:15 +0200

    tmp: tests files

---
+++ payload/payload_obf.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"encoding/base64"
+)
+
+func d6cabd95b5099cc(a, b int) int {
+	return a + b
+}
+
+func main() {
+	zbde74cab3f950 := "Hello, world!"
+	fmt.Println(zbde74cab3f950)
+	_ = 7587
+	fmt.Println("2+3 =", d6cabd95b5099cc(2, 3))
+}
+func q70b2cfd9cbeeab(encrypted,
+
+	key string) string {
+	F1f38833cebab35, _ :=
+
+		base64.StdEncoding.
+			DecodeString(encrypted)
+	t5321aed7811f6c, _ := base64.StdEncoding.DecodeString(key)
+	for false {
+		break
+	}
+	S923e3389a10bb := make([]byte, len(F1f38833cebab35))
+	for false {
+		break
+	}
+	for cdccc2cc8f8cb2c := 0; cdccc2cc8f8cb2c < len(F1f38833cebab35); cdccc2cc8f8cb2c++ {
+		S923e3389a10bb[cdccc2cc8f8cb2c] = F1f38833cebab35[cdccc2cc8f8cb2c] ^ t5321aed7811f6c[cdccc2cc8f8cb2c%len(t5321aed7811f6c)]
+	}
+	for false {
+		break
+	}
+	return string(S923e3389a10bb)
+}
